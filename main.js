d63522c291b9881b0c50abf411806b441de8802d
LakshyaJain28 <79844523+LakshyaJain28@users.noreply.github.com>
2021-11-17 16:21:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,41 @@ Webcam.attach('#camera');
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML= '<img id="result" src="'+data_uri+'">';
+        document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'">';
     });
 }
 
 console.log('ml5 version', ml5.version);
 
-classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zJyuAQ-0x/model.json',modelLoaded);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lZDDODVUs/model.json',modelLoaded);
 
 function modelLoaded()
 {
     console.log("model loaded !!")
+}
+function check()
+{
+    img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error,results)
+{
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML=results[0].label;
+        prediction_1=results[0].label;
+        speak();
+        if(results[0].label=="Best"){
+            document.getElementById("update_emoji").innerHTML="&#128076;";
+        }
+        if(results[0].label=="Victory"){
+            document.getElementById("update_emoji").innerHTML="&#9996;";
+        }
+        if(results[0].label=="Amazing"){
+            document.getElementById("update_emoji").innerHTML="&#128077;";
+        } 
+    }
 }
